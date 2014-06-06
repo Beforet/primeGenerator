b175dc25b2f0b00347fde211a6ca9e25391670d8
Beforet <t.beforet@gmail.com>
2014-06-05 23:22:45 -0400

    First Version
    I've got code, but no results to speak of.

---
+++ primeGenerator.java
@@ -0,0 +1,49 @@
+/**
+Prime Generator
+A program that generates prime numbers up to a user defined number.
+*/
+import java.util.ArrayList;
+public class primeGenerator {
+	private ArrayList<Integer> primes;
+	public primeGenerator(){
+		primes = new ArrayList<Integer>();
+		primes.add(1);
+		primes.add(2);
+	}
+	/**
+	Tests to see if a number is a prime number by testing it against a list of other prime numbers. If none of the other prime numbers is a number, then the number must be prime and the method
+	returns true.
+	*/
+	private boolean isPrime(int testNo){
+		boolean prime = false;
+		if(testNo == 1 || testNo == 2){
+			prime = true;
+			return prime;
+		}
+		for(Integer number : primes){
+			int n = number;
+			if(n > testNo)
+				break;
+			if(testNo % n == 0){
+				if(testNo == n)
+					prime = true;
+				else
+					prime = false;
+			}
+		}
+		if(prime == true){
+			if(primes.indexOf(testNo) < 0)
+				primes.add(testNo);
+		}
+		return prime;
+	}
+	/**
+	Generates and prints all the prime numbers between 0 and a user defined number.
+	*/
+	public void generate(int target){
+		for(int i = 1; i <= target; i++){
+			if(isPrime(target))
+				System.out.println(target);
+		}
+	}
+}
---
+++ primeGeneratorTest.java
@@ -0,0 +1,6 @@
+public class primeGeneratorTest{
+	public static void main(String[] args){
+		primeGenerator generator = new primeGenerator();
+		generator.generate(10);
+	}
+}
